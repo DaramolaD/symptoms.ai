6e6d59e229589d96d66232edc3034d38cbd653c0
Daramola Daniel <dandaramola.d@gmail.com>
2023-11-27 15:27:25 +0100

    chat_layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import styles from "../styles/loading-dots.module.css";
+import "../assets/chat.css"
 import Header from '@/components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
